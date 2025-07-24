4f7fe138758775f0caddb43d2f68d779018580dc
Encoded <brandonjkkim@gmail.com>
2025-07-24 14:00:55 -0600

    commented ot

--- src/auton/awp.cpp
+++ src/auton/awp.cpp
@@ -1,42 +1,42 @@
 #include "main.h"
 
-void sawp() {
+void sawp() 
 // libKS MTPoint v0.1
 // Starting point: (54.83 in, 12.77 in)
-chassis.moveToPoint(0.00, 0.00, 500, {.forwards = true, .maxSpeed = 127, .minSpeed = 0}); // Point 1
-liftPiston.set_value(true); // Lift piston up
-chassis.moveToPoint(-5.00, 30.83, 3000, {.forwards = true, .maxSpeed = 127, .minSpeed = 0}); // Point 2
-chassis.turnToHeading(90, 1000); // Turn to 90 degrees
-loaderPiston.set_value(true); // Loader piston up
-intake.move_voltage(12000); // Spin intake
-chassis.moveToPoint(2.315, 30.33, 1200, {.forwards = true, .maxSpeed = 127, .minSpeed = 127}); // Point 3
-pros::delay(950);
-chassis.moveToPoint(-4.15, 32.00, 2000, {.forwards = false, .maxSpeed = 127, .minSpeed = 0}); // Point 4
-chassis.turnToHeading(270, 1000); // Turn to 270 degrees
-pros::delay(500);
-loaderPiston.set_value(false); // Loader piston down
-intake.move_voltage(12000); // Reverse intake
-chassis.moveToPoint(-26.00, 36.13, 2000, {.forwards = true, .maxSpeed = 127, .minSpeed = 0}); // Point 4
-pros::delay(500);
-trapdoorPiston.set_value(true); // Trapdoor piston up
-intake.move_voltage(-4000); // Stop intake
-pros::delay(500);
-intake.move_voltage(12000); // Reverse intake
-pros::delay(1800);
-chassis.moveToPoint(-17.45, 30.83, 2000, {.forwards = false, .maxSpeed = 127, .minSpeed = 127}); // Point 5
-chassis.turnToHeading(225, 1000); // Turn to 225 degrees
-chassis.moveToPoint(-33.83, 9.66, 3000, {.forwards = true, .maxSpeed = 40, .minSpeed = 0}); // Point 6
-chassis.moveToPoint(-50.22, -3.92, 1000, {.forwards = true, .maxSpeed = 127, .minSpeed = 0}); // Point 7
-pros::delay(700);
-intake.move_voltage(-7600); // Stop intake
-pros::delay(500);
-chassis.turnToHeading(225, 1000); // Turn to 0 degrees
-pros::delay(2000);
-chassis.moveToPoint(-31, 3, 1000, {.forwards = false});
-chassis.moveToPoint(-31.55, -37.34, 2000, {.forwards = true, .maxSpeed = 127, .minSpeed = 0}); // Point 8
-chassis.turnToHeading(315, 1000);
-chassis.moveToPoint(-42.49, -29.40, 2000, {.forwards = true, .maxSpeed = 127, .minSpeed = 0}); // Point 9
-// chassis.moveToPoint(-4.51, -58.59, 2000, {.forwards = true, .maxSpeed = 127, .minSpeed = 0}); // Point 10
+// chassis.moveToPoint(0.00, 0.00, 500, {.forwards = true, .maxSpeed = 127, .minSpeed = 0}); // Point 1
+// liftPiston.set_value(true); // Lift piston up
+// chassis.moveToPoint(-4, 31.83, 3000, {.forwards = true, .maxSpeed = 127, .minSpeed = 0}); // Point 2
+// chassis.turnToHeading(90, 1000); // Turn to 90 degrees
+// loaderPiston.set_value(true); // Loader piston up
+// intake.move_voltage(12000); // Spin intake
+// chassis.moveToPoint(4.915, 31.53, 1200, {.forwards = true, .maxSpeed = 127, .minSpeed = 127}); // Point 3
+// pros::delay(1150);
+// chassis.moveToPoint(-4.15, 30.90, 2000, {.forwards = false, .maxSpeed = 127, .minSpeed = 0}); // Point 4
+// chassis.turnToHeading(270, 1000); // Turn to 270 degrees
+// pros::delay(500);
+// loaderPiston.set_value(false); // Loader piston down
+// intake.move_voltage(12000); // Reverse intake
+// chassis.moveToPoint(-24.70, 34.63, 2000, {.forwards = true, .maxSpeed = 127, .minSpeed = 0}); // Point 4
+// pros::delay(500);
+// trapdoorPiston.set_value(true); // Trapdoor piston up
+// intake.move_voltage(-5000); // Stop intake
+// pros::delay(500);
+// intake.move_voltage(12000); // Reverse intake
+// pros::delay(2100);
+// chassis.moveToPoint(-17.45, 30.83, 2000, {.forwards = false, .maxSpeed = 127, .minSpeed = 127}); // Point 5
+// chassis.turnToHeading(225, 1000); // Turn to 225 degrees
+// chassis.moveToPoint(-35.83, 10.66, 3000, {.forwards = true, .maxSpeed = 40, .minSpeed = 0}); // Point 6
+// chassis.moveToPoint(-47.52, -4.62, 1000, {.forwards = true, .maxSpeed = 127, .minSpeed = 0}); // Point 7
+// pros::delay(700);
+// intake.move_voltage(-5000); // Stop intake
+// pros::delay(500);
+// chassis.turnToHeading(225, 1000); // Turn to 0 degrees
+// pros::delay(2500);
+// chassis.moveToPoint(-31, 3, 1000, {.forwards = false});
+// chassis.moveToPoint(-31.55, -37.34, 2000, {.forwards = true, .maxSpeed = 127, .minSpeed = 0}); // Point 8
+// chassis.turnToHeading(315, 1000);
+// chassis.moveToPoint(-42.49, -29.40, 2000, {.forwards = true, .maxSpeed = 127, .minSpeed = 0}); // Point 9
+// // chassis.moveToPoint(-4.51, -58.59, 2000, {.forwards = true, .maxSpeed = 127, .minSpeed = 0}); // Point 10
 // chassis.moveToPoint(7.73, -59.23, 2000, {.forwards = true, .maxSpeed = 127, .minSpeed = 0}); // Point 11
 // chassis.moveToPoint(-27.04, -59.23, 2000, {.forwards = true, .maxSpeed = 127, .minSpeed = 0}); // Point 12
 
