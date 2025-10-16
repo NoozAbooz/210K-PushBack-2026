6513097b8b9a1c9f6eb1cd510b1b7eb54d3522de
Michael Zheng <zhengm58@gmail.com>
2025-10-15 22:53:35 -0600

    ahhhhhhhhhh

--- src/subsystem/telemetry.cpp
+++ src/subsystem/telemetry.cpp
@@ -1,6 +1,7 @@
 #include "main.h"
 
-bool telemToggle = true; // for switching tele output on controller screen
+int telemState = 0; // for switching tele output on controller screen
+int timer = 0;
 std::string rumble_pattern = "";
 
 void initTelemetry() {
@@ -8,13 +9,25 @@ void initTelemetry() {
 		while (true) {
 			// Report temperature telemetry 😭
 			double drivetrainTemps = kw::vector_average(leftDrive.get_temperature_all());
-			if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_UP)) {
-				telemToggle = !telemToggle; // Toggle telemetry display
+			if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_LEFT)) {
+				telemState = (telemState + 1) % 2; // Toggle telemetry display
 			}
-			if(!telemToggle) {
+
+			// if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_UP)) {
+			// 	gui_selector.next_auton();
+			// }
+			// if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_DOWN)) {
+			// 	gui_selector.prev_auton();
+			// }
+
+			if(telemState == 0) {
 				controller.print(0, 0, "DT%.0lf|INT%.0lf|T%.0lf  ", drivetrainTemps, intakeBottom.get_temperature(), kw::theta);
-			} else {
+			} else if (telemState == 1) {
 				controller.print(0, 0, "X:%.0lf Y:%.0lf T:%.0lf   ", kw::x_pos, kw::y_pos, kw::theta);
+			} else if (telemState == 2) {
+				auto auton = gui_selector.get_auton();
+				const char* auton_name = auton->name.c_str();
+				controller.print(0, 0, "%s|%s", alliance.c_str(), auton_name);
 			}
 
 			pros::delay(50);
