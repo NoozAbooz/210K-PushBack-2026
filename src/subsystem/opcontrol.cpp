d1917f00c9d32838437570a11e20654b7670dbbb
Encoded <brandonjkkim@gmail.com>
2025-12-20 16:28:10 -0700

    new stuff

--- src/subsystem/opcontrol.cpp
+++ src/subsystem/opcontrol.cpp
@@ -20,7 +20,7 @@ void intakeMacro(std::string str) {
 		intakeMiddleLower.move_voltage(12000);
 		intakeTop.move_voltage(12000);
 	} else if (str == "R2") { // score on mid goal
-		intakeTop.move_voltage(-3000);
+		intakeTop.move_voltage(-2800);
 		intakeMiddleLower.move_voltage(-500);
 		intakeBottom.move_voltage(12000);
 	} else if (str == "L1") { // intake up to long goal scoring
