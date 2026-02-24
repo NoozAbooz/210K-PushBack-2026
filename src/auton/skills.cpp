4fb40e0b592dbbd19d2bd17e96081b4b14200fd5
Brandon Kim <105524304+Sir-Encoded@users.noreply.github.com>
2026-02-24 13:29:48 -0700

    framing skills

--- src/auton/skills.cpp
+++ src/auton/skills.cpp
@@ -1,101 +1,43 @@
 #include "main.h"
 
 void skills(){
-    toggleColourSort = false;
+    // libKW MTPoint v0.1
+// Starting point: (-43.81 in, 13.17 in)
+ kw::moveToPoint(0.00, 0.00, 2000); // Start Same as Left Elims
+ kw::moveToPoint(-5.58, 21.16, 2000 ); // Move To Four Stack
+ kw::moveToPoint(-1.68, 34.31, 2000, false); // mid goal
+ //Intake mid here
+ kw::moveToPoint(-31.86, -1.84, 2000 ); // loader movement
+ kw::moveToPoint(-31.86, -14.71, 2000 ); // loading
+ kw::moveToPoint(-45.96, 23.29, 2000 ); // boomerang to alley
+ kw::moveToPoint(-46.57, 82.11, 2000 ); // move straight in alley
+ //turn here keeping loader down
+ kw::moveToPoint(-32.48, 82.11, 2000 ); // align with goal
+ //turn to goal
+ kw::moveToPoint(-33.09, 71.08, 2000 ); // at goal now  
+ //score here
+ kw::moveToPoint(-31.86, 106.01, 2000 ); // loader
+ kw::moveToPoint(-33.09, 71.69, 2000 ); // score
+// look at very bottom and reset after this second set of loading
+// this motion is good for from the long goal to the park zone and make sure to tune htis
+    // loaderPiston.set_value(false);
+    // // kw::set_odom_position(0, 0, 0);
+    // kw::boomerang(33, 34, 90, 0.4, 2000, 70);
+    // loaderPiston.set_value(true);
+    // wingPiston.set_value(true);
+    // pros::delay(400);
+    // kw::move_raw(8000, 8000);
+    // pros::delay(1400);
+    // loaderPiston.set_value(false);
+    // kw::move_raw(6000, 6000);
+    // pros::delay(700);
+    // kw::move_raw(0, 0);
 
-	intakeMacro("L1");
-    wingPiston.set_value(true); // deploy wings for driver
-    blockerPiston.set_value(true);
-
-    double reset_x_coord;
-	double reset_y_coord;
-
-	kw::move_raw(9000, 9000);
-    pros::delay(400);
-    
-    kw::move_raw(-4000, -4000);
-    pros::delay(200);
-    kw::move_raw(12000, 12000);
-    pros::delay(600);
-    kw::move_raw(-4000, -4000);
-    pros::delay(300);
-    kw::move_raw(10000, 10000);
-    pros::delay(500);
-    
-    kw::turnToAngle(0, 500);
-    
-    loaderPiston.set_value(true);
-    kw::move_raw(-12000, -12000);
-    pros::delay(650);
-    kw::stop_chassis(pros::E_MOTOR_BRAKE_HOLD);
-    kw::turnToAngle(0, 500);
-    wingPiston.set_value(false);
-    kw::stop_chassis(pros::E_MOTOR_BRAKE_HOLD);
-    pros::delay(300);
-
-    // dist reset #1
-    reset_x_coord = -(kw::getDistance(rightDistance) - 65.4);
-	reset_y_coord = -kw::getDistance(fwdDistance);
-    kw::set_odom_position(reset_x_coord, reset_y_coord);
-    console.printf("rightDist: %.2f fwdDist: %.2f\n", kw::getDistance(rightDistance), kw::getDistance(fwdDistance));
-    console.printf("#1 resetX: %.2f resetY: %.2f\n", reset_x_coord, reset_y_coord);
-    loaderPiston.set_value(false);
-
-    // move to midgoal
-    kw::moveToPoint(9.5, -64.8, 1500, false);
-    kw::swing(45, 250, false);
-
-    // // grab 1 blue ball, score all 7
-    kw::driveTo(9, 900, 90);
-    // kw::moveToPoint(17, -64, 1500, true);
-    // kw::moveToPoint(11, -70,1000, false);
-    kw::driveTo(-12, 800);
-    intakeMacro("R2");
-    kw::move_raw(-7000, -7000);
-
-    // slow down intake over time to prevent straggling balls in intake
-    intake_velocity.set_target(520);
-    pros::delay(1000);
-    intake_velocity.set_target(390);
-
-    pros::delay(2500);
-    pros::Task([] {
-        pros::delay(500);
-        loaderPiston.set_value(true);
-        //pros::delay(1000);
-        intakeMacro("L1");
-    });
-    kw::moveToPoint(48, -30, 2000);
-    kw::turnToAngle(0, 700);
-    kw::moveToPoint(47, -23, 1000, true);
-    kw::move_raw(5000, 5000);
-    pros::delay(1000);
-    kw::boomerang(64, -72, 0, 0.5, 2000, false, 90);
-    intakeMacro("stop");
-    kw::moveToPoint(60, -119, 1000, false, 127, false);
-    // kw::boomerang(45, -119, 45, 0.5, 2000, false, 60);
-    kw::turnToAngle(270, 1000);
-    kw::moveToPoint(50, -122, 1000, true, 127);
-    kw::turnToAngle(180, 1000);
-    kw::moveToPoint(48.6, -108, 1000, false, 80);
-    intakeMacro("L2");
-    pros::delay(200);
-    intakeMacro("R1");
-    kw::move_raw(-8000, -8000);
-    pros::delay(2500);
-    intakeMacro("L1");
-    kw::moveToPoint(50.6, -136, 1000, true, 80);
-    kw::move_raw(6000, 6000);
-    pros::delay(1000);
-    kw::moveToPoint(48.6, -108, 1000, false, 80);
-    pros::delay(100);
-     intakeMacro("R1");
-    kw::move_raw(-8000, -8000);
-    pros::delay(2000);
+    // from here its reset and you will have to do everything from here on out
 
 
 
-
+// this motion is good for from the long goal to the park zone and make sure to tune htis
     // loaderPiston.set_value(false);
     // // kw::set_odom_position(0, 0, 0);
     // kw::boomerang(33, 34, 90, 0.4, 2000, 70);
