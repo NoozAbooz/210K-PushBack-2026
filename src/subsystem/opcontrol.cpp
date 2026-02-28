424aa299d6b243f4fcd95e4ac6b7678217f1faa6
Michael Zheng <44128563+NoozAbooz@users.noreply.github.com>
2026-02-28 13:33:03 -0700

    yodates

--- src/subsystem/opcontrol.cpp
+++ src/subsystem/opcontrol.cpp
@@ -20,7 +20,7 @@ void intakeMacro(std::string str) {
 		intake_velocity.set_target(12000);
 	} else if (str == "R2") { // score on mid goal
 		intakePullDownPiston.set_value(true); // pull down intake band
-		intake_velocity.set_target(550);
+		intake_velocity.set_target(480);
 		
 	} else if (str == "L1") { // intake up to long goal scoring
 		trapdoorPiston.set_value(false); // close trapdoor
