69b8670501c613a9f0229c47137f7a446ac9acef
Brandon Kim <105524304+Sir-Encoded@users.noreply.github.com>
2026-04-14 12:25:18 -0600

    minor fix

--- src/auton/awp.cpp
+++ src/auton/awp.cpp
@@ -27,7 +27,7 @@ void true_sawp(){
 	intakeMacro("L1");
 	kw::moveToPoint(24.4, -7.0, 1000, true, 90); // Mid Cluster 1
 	kw::turnToAngle(88, 500, 127, 0, false);
-	kw::moveToPoint(66, -6.2, 1000, true, 127, false); // Mid Cluster 1
+	kw::moveToPoint(68, -6.2, 1000, true, 127, false); // Mid Cluster 1
 	kw::boomerang(100.5, 23, 0, 0.05, 2000, true, 85); // Mid Cluster 2
 	pros::Task([] {
 		pros::delay(450);
