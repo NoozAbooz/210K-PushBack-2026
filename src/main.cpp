d92dcec7d250c5e1ee7a714dc4b9d11240cce58d
Brandon Kim <105524304+Sir-Encoded@users.noreply.github.com>
2026-04-17 09:46:28 -0600

    daniel tuuning drive

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ void opcontrol() {
 
 	while (true) { // Main continuous loop
 		/* Drive */
-		kw::drive_arcade(0, 0, 0.82);
+		kw::drive_arcade(0, 0, 0.9);
 
 		/* Subsystem Listeners */
 		refreshSubsys1(); // intake
